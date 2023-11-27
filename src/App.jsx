9ecb423b3ce8545d11e4f443e2a5a7f42cf231b0
jdmedlock <jdmedlock@gmail.com>
2023-11-27 17:01:47 -0600

    Feature: Add jschacha algorithm [WIP]

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import chinguLogo from './assets/Chingu_Logo_small.png'
+import { randomBytes } from 'randombytes'
+import JSChaCha20 from './util/jschacha20.js'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let randomNos = []
 
+const generateRandomNos = (numberToGenerate) => {
+  // Since crypto.randomBytes is only used in NodeJS rely instead on nanoid
+  const key = new Uint8Array(randomBytes(32))
+  const nonce = new Uint8Array(randomBytes(12))
+  const data = new Uint8Array(randomBytes(4096))
+
+  for (let i = 0; i < numberToGenerate; i++) {
+    const encoder = new JSChaCha20(key, nonce)
+    const encr = encoder.encrypt(data)
+
+    // Generate a random number in the range 0-100
+    const rn = parseInt(encr) % 101
+    randomNos.push(rn)
+    console.log('rn: ', rn)
+  }
+}
+
+const DisplayRandomNos = (randomNos) => {
+  return randomNos.map(randomNo =>
+    <li key={ randomNo }></li>
+  )
+}
+
+const App = () => {
+  generateRandomNos(100)
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={ chinguLogo } className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Chingu Roundtable - Implementing & Evaluating a CSPRNG</h1>
+      <div>
+        <DisplayRandomNos/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
