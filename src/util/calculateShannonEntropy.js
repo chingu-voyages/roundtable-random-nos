c125f9abf46e463bb167278ff77e9018dd126066
jdmedlock <jdmedlock@gmail.com>
2023-12-01 08:16:40 -0600

    refactor: Add new Shannon entropy algorithm based on arrays instead of strings

--- src/util/calculateShannonEntropy.js
+++ src/util/calculateShannonEntropy.js
@@ -1,29 +1,4 @@
 const calculateShannonEntropy = (randomNos) => {
-  /*
-  // Convert the input array of random numbers into a string
-  const inputString = randomNos.join('')
-
-  // Count occurrences of each symbol in the input string
-  const frequencies = {};
-  for (let i = 0; i < inputString.length; i++) {
-    const symbol = inputString[i];
-    if (frequencies[symbol]) {
-      frequencies[symbol]++;
-    } else {
-      frequencies[symbol] = 1;
-    }
-  }
-
-  // Calculate entropy
-  let entropy = 0;
-  const length = inputString.length;
-  for (const symbol in frequencies) {
-    const probability = frequencies[symbol] / length;
-    entropy -= probability * (Math.log2(probability));
-  }
-
-  return entropy;
-  */
   const frequency = {}
   const totalElements = randomNos.length
 
